2de76e9fd017590837be1dc81564a2b7b3b5a9a9
lllllinli <standleylin@gmail.com>
2016-08-15 10:53:22 +0800

    update

---
+++ es5/Validate.js
@@ -0,0 +1,42 @@
+"use strict";
+var VALIDATE = VALIDATE || {};
+
+/*
+* VALIDATE.check({type:'E-MAIL'}, 'name@gmail.com', function(res){}); 
+*
+*/
+VALIDATE.check = (function() {
+  var _init       = function() {},
+      _checkType  = {},
+      _check      = function() {},
+      _checkEmail = function() {};
+
+  _init = function() {
+    _checkType = {
+      'E-MAIL':'E-MAIL'
+    };
+  };
+
+  _check = function(options, checkVale, checkCallback) {
+    switch (options.type) {
+      case _checkType['E-MAIL']:
+        _checkEmail(checkVale, checkCallback);
+        break;
+      default:
+    }
+  };
+
+  _checkEmail = function(value, callback) {
+    /**
+    * regex source: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+    **/
+    var _isEmailAddr = false,
+        _re          = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    _isEmailAddr = _re.test(value);
+    callback(_isEmailAddr);
+  };
+  // 初始化
+  _init();
+
+  return _check;
+})();
---
+++ test/test.js
@@ -0,0 +1,5 @@
+var obj = require('../index');
+var getNickname = obj['getNickname'];
+console.log(obj);
+console.log(getNickname);
+console.log(getNickname());
